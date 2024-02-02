a0a7ab735718ad487ddb18084f5f490f944c04f7
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-02 14:57:33 +0400

    pass js 02 exercise first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let str = "";
+  while (num > 0) {
+    str += string;
+    num--;
+  }
+  return str;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
