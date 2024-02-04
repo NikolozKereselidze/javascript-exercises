fdc0c653d1ff37d752bbd8c3f1b8429d931ed60d
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-04 15:26:43 +0400

    pass js 05 exercise fifth test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,12 @@ const sumAll = function (num1, num2) {
   let min;
   let max;
 
-  if (num1 < 0 || num2 < 0) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
     return "ERROR";
   } else if (num1 >= num2) {
     min = num2;
