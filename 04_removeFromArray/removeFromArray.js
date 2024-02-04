27db3a69412a1c00ef03bf3ce105b2756ddb7836
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-04 14:56:02 +0400

    pass js 04 exercise first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...arrNumber) {
+  return arr.filter((x) => !arrNumber.includes(x));
 
+  //   return array.filter((x) => !args.includes(x));
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
