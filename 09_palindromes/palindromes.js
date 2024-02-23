8bcefb9c339080e4f8e2f3065d912b0c340a2a5b
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-23 16:40:39 +0400

    finish 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const str = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return str.split("").reverse().join("") === str;
 };
 
 // Do not edit below this line
