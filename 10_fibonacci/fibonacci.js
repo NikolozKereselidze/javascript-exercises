ef3a8b703ffa185a538b919739badf028dee649b
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-23 19:11:48 +0400

    finish 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (Number(num) === 0) return 0;
 
+  let sum = 0;
+  let num1 = 0;
+  let num2 = 1;
+  for (let i = 2; i <= num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 };
 
 // Do not edit below this line
