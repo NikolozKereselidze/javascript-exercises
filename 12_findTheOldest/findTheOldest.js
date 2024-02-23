21635bc6d566fc581ea8dd831b01ca92f83ec7bd
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-23 19:38:26 +0400

    finish 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldest, cur) => {
+    const currentAge = cur.yearOfDeath
+      ? cur.yearOfDeath - cur.yearOfBirth
+      : new Date().getFullYear() - cur.yearOfBirth;
 
+    const oldestAge = oldest.yearOfDeath
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : new Date().getFullYear() - oldest.yearOfBirth;
+
+    return currentAge > oldestAge ? cur : oldest;
+  });
 };
 
 // Do not edit below this line
